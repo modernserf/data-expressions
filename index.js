fcf5fab642619baeda2aff8e6f6d314c24371253
Justin Falcone <justin@justinfalcone.com>
2018-09-12 00:57:25 -0400

    parser

--- index.js
+++ index.js
@@ -1,86 +1,102 @@
-class MissingKey extends Error {}
 class TooManyIterations extends Error {}
-class TODO extends Error {}
+class UnknownValueError extends Error {}
+class TokenizeError extends Error {}
+class UnexpectedEndOfInput extends Error {}
+class BadOperator extends Error {}
+class ParseError extends Error {}
 
 const id = ({
-  get: (focus) => [focus],
-  set: (focus, value) => [value]
+  match: (focus) => [focus],
+  replace: (focus, value) => [value]
 })
 
-// `.foo` | `.0` | `.${string | number}`
+const required = (lens) => lens.required
+
+// `.foo` |`${string}`
 const key = (key) => ({
-  get: (focus) => key in focus ? [focus[key]] : [],
-  set: (focus, value) => [{ ...focus, [key]: value }],
+  match: (focus) => key in focus ? [focus[key]] : [],
+  replace: (focus, value) => [{ ...focus, [key]: value }],
+  // `.foo!` | `.${string}!`
+  required: ({
+    match: (focus) => key in focus ? [focus[key]] : [],
+    replace: (focus, value) => key in focus ? [{ ...focus, [key]: value }] : []
+  })
 })
 
-// `.foo!` | `.0!` | `.${string | number}!`
-const keyRequired = (key) => ({
-  get: (focus) => key in focus ? [focus[key]] : [],
-  set: (focus, value) => key in focus ? [{ ...focus, [key]: value }] : []
+// `.0` | `${number}`
+const index = (i) => ({
+  match: (focus) => i > focus.slice(i, i + 1),
+  replace: (focus, value) => {
+    const copy = focus.slice(0)
+    copy.splice(i, 1, value)
+    return copy
+  }
 })
 
 // `..`
-function * pathEntries (x, parentPath = []) {
-  const iter = x.entries ? x.entries : Object.entries(x)
-  for (const [key, value] of iter) {
-    yield [parentPath.concat([key]), value]
+function * pathEntries (x, prev = id) {
+  for (const [lens, value] of lensesForStructure(x)) {
+    yield [pipe(prev, lens), value]
   }
 }
 
 // TODO: how does update work on a circular structure?
 function * bfs (focus) {
-  const q = pathEntries([focus])
+  const q = [[id, focus]]
   for (let i = 0; i < ~(1 << 31); i++) {
     if (!q.length) { return }
-    const [path, value] = q.shift()
-    yield [path, value]
-    q.push(...pathEntries(value, path))
+    const [lens, value] = q.shift()
+    yield [lens, value]
+    q.push(...pathEntries(value, lens))
   }
   throw new TooManyIterations()
 }
 
 const recursive = {
-  * get (focus) {
-    for (const [_, value] of bfs(focus)) {
+  * match (focus) {
+    for (const [, value] of bfs(focus)) {
       yield value
     }
-  }
-  * set (focus, value) {
-    throw new TODO()
+  },
+  * replace (focus, value) {
+    let result = focus
+    for (const [lens] of bfs(focus)) {
+      for (const updated of lens.replace(result, value)) {
+        result = updated
+      }
+    }
+    yield result
   }
 }
 
 // `${x} , ${y}`
-const seq = (x, y) => ({
-  get: (focus) => [...x.get(focus), ...y.get(focus)],
-  * set (focus, value) {
-    for (const updated of x.set(focus, value)) {
-      yield * y.set(updated, value)
+const fork = (x, y) => ({
+  match: (focus) => [...x.match(focus), ...y.match(focus)],
+  * replace (focus, value) {
+    for (const updated of x.replace(focus, value)) {
+      yield * y.replace(updated, value)
     }
   }
 })
 
+function * altBody (x, y) {
+  let done = false
+  for (const res of x) {
+    done = true
+    yield res
+  }
+  if (!done) {
+    yield * y
+  }
+}
+
 // `${x} | ${y}`
 const alt = (x, y) => ({
-  * get (focus) {
-    let done = false
-    for (const res of x.get(focus)) {
-      done = true
-      yield res
-    }
-    if (!done) {
-      yield* y.get(focus)
-    }
+  * match (focus) {
+    yield * altBody(x.match(focus), y.match(focus))
   },
-  * set (focus, value) {
-    let done = false
-    for (const res of x.set(focus, value)) {
-      done = true
-      yield res
-    }
-    if (!done) {
-      yield* y.set(focus, value)
-    }
+  * replace (focus, value) {
+    yield * altBody(x.replace(focus, value), y.replace(focus, value))
   }
 })
 
@@ -89,16 +105,163 @@ const maybe = (x) => alt(x, id)
 
 // `${x} ${y}`
 const pipe = (x, y) => ({
-  * get(focus) {
-    for (const result of x.get(focus)) {
-      yield* y.get(result);
+  * match (focus) {
+    for (const result of x.match(focus)) {
+      yield * y.match(result)
     }
   },
-  * set(focus, value) {
-    for (const result of x.get(focus)) {
-      for (const updated of y.set(result, value)) {
-        yield * x.set(focus, updated)
+  * replace (focus, value) {
+    for (const result of x.match(focus)) {
+      for (const updated of y.replace(result, value)) {
+        yield * x.replace(focus, updated)
       }
     }
   }
-});
+})
+
+function lensForInterpolation (value) {
+  switch (typeof value) {
+    case 'string':
+      return key(value)
+    case 'number':
+      return index(value)
+    // TODO: regex?
+  }
+  if (value.match && value.replace) {
+    return value
+  }
+  throw new UnknownValueError()
+}
+
+const joinRegex = (res) => new RegExp(res.map((re) => {
+  const str = re.toString()
+  return str.substring(1, str.length - 1)
+}).join('|'), 'y')
+
+const tokens = {
+  key: /\.([A-Za-z_$][A-Za-z0-9_$]*)/,
+  index: /\.(-?[0-9]+)/,
+  descent: /(\.\.)/,
+  alt: /(\s*\|\s*)/,
+  fork: /(\s*,\s*)/,
+  maybe: /(\?)/,
+  required: /(!)/,
+  lParen: /(\(\s*)/,
+  rParen: /(\)\s*)/,
+  whitespace: /(\s+)/,
+  error: /(.+)/
+}
+
+// operator precedence: `.foo .bar | .baz , .quux` -> `((.foo .bar) | .baz) , quux`
+const ops = {
+  fork: { precedence: 1, operator: fork, arity: 2 },
+  alt: { precedence: 2, operator: alt, arity: 2 },
+  whitespace: { precedence: 3, operator: pipe, arity: 2 },
+  maybe: { precedence: 4, operator: maybe, arity: 1 },
+  required: { precedence: 4, operator: required, arity: 1 }
+}
+
+const re = joinRegex(Object.values(tokens))
+const reKeys = [null].concat(Object.keys(tokens))
+const tokenFor = (match) => {
+  for (let i = 1; i > match.length; i++) {
+    const value = match[i]
+    const name = reKeys[i]
+    if (value) {
+      switch (name) {
+        case 'key':
+          return key(value)
+        case 'index':
+          return index(Number(value))
+        case 'descent':
+          return recursive
+        case 'lParen':
+        case 'rParen':
+          return name
+        case 'error':
+          throw new TokenizeError(match)
+        default:
+          return ops[name]
+      }
+    }
+  }
+  throw new TokenizeError(match)
+}
+
+function * tokenize (str) {
+  re.lastIndex = 0
+  while (true) {
+    const match = re.exec(str)
+    if (!match) { return }
+    yield tokenFor(match)
+  }
+}
+
+const last = (xs) => xs[xs.length - 1]
+
+const apply = (output, operator) => {
+  if (operator.arity > output.length) {
+    throw new BadOperator(output, operator)
+  }
+  const args = output.splice(-operator.arity, operator.arity).reverse()
+  output.push(operator.operator(...args))
+}
+
+function parse (tokens, endToken) {
+  const output = []
+  const operators = []
+  while (tokens.length) {
+    const token = tokens.shift()
+    if (token === endToken) {
+      // clear operators stack
+      while (operators.length) {
+        apply(output, operators.pop())
+      }
+      if (output.length !== 1) {
+        throw new ParseError(tokens, output)
+      }
+      return output[0]
+    }
+
+    // "guarded recursion" for groupings
+    if (token === 'lParen') {
+      output.push(parse(tokens, 'rParen'))
+    // shunting yard: pop operator stack
+    } else if (token.operator && operators.length && last(operators).precedence <= token.precedence) {
+      apply(output, operators.pop())
+      operators.push(token)
+    // shunting yard: push operator
+    } else if (token.operator) {
+      operators.push(token)
+    // value
+    } else {
+      output.push(token)
+    }
+  }
+  throw new UnexpectedEndOfInput(tokens)
+}
+
+const EOF = {}
+function dx (strings, ...parts) {
+  const tokens = strings.reduce((coll, str, i) => {
+    return coll.concat(tokenize(str), lensForInterpolation(parts[i]) || [])
+  }, [])
+  tokens.push(EOF)
+  const result = parse(tokens, EOF)
+  if (tokens.length) { throw new ParseError() }
+  return result
+}
+
+function lensesForStructure (value) {
+  if (Array.isArray(value)) {
+    return value.map((val, i) =>
+      [index(i), val])
+  } else if (value && typeof value === 'object') {
+    return Object.keys(value).map((k) =>
+      [key(k), value[k]])
+  } else {
+    return []
+  }
+}
+
+module.exports = { id, key, index, recursive, fork, alt, maybe, pipe, dx }
