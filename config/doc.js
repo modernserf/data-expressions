77589450f64db69cd4881f476090e694d1528fa4
Justin Falcone <justin.falcone@buzzfeed.com>
2018-09-17 16:04:14 -0400

    test formatting

--- config/doc.js
+++ config/doc.js
@@ -30,22 +30,63 @@ const stylesheet = css`
   body {
     margin: 0 auto;
     max-width: 800px;
+    font-family: sans-serif;
+  }
+  p {
+    margin: 1em auto 0.5em;
   }
   .test {
     border: 1px solid #ccc;
-    padding: 1em;
+    padding-bottom: 1em;
   }
   code {
     display: block;
     white-space: pre;
-    line-height: 0.7;
+    line-height: 1.4em;
+  }
+  section {
+    margin: 1em 0;
+  }
+  .test header {
+    background-color: papayawhip;
+    font-weight: bold;
+    padding: 0.5em;
+  }
+  .test .container {
+    padding: 0.5em;
+  }
+  summary code {
+    display: inline;
+    white-space: normal;
+  }
+  .test code {
+    padding-left: 1em;
+    position: relative;
+    white-space: pre-line;
+  }
+  .expect::before,
+  .to-equal::before {
+    position: absolute;
+    left: 0;
+    font-weight: bold;
+  }
+  .expect::before {
+    content: ">";
+    color: blue;
+  }
+  .to-equal::before {
+    content: "<";
+    color: gray;
+  }
+  .comment {
+    line-height: 1.4;
   }
   .comment code {
     display: inline-block;
     background-color: #eee;
-  }
-  section {
-    margin: 1em 0;
+    line-height: 1;
+    font-size: 16px;
+    padding-left: 0;
   }
 `
 
@@ -63,58 +104,95 @@ function * formatFile (name, data) {
 const removeEmpty = (line) => /\S/.test(line)
 
 function * comment (lines) {
-  yield `<aside class="comment">`
-  let buffer = ''
+  const buffer = []
   while (lines.length) {
     const line = lines[0]
     let match = line.match(commentPattern)
     if (match) {
-      buffer += match[1] + '\n'
+      buffer.push(match[1])
       lines.shift()
     } else {
       break
     }
   }
-  yield marked(buffer)
-  yield `</aside>`
+  yield * h('aside', 'class="comment"', marked(buffer.join('\n')))
 }
 
 const indentedPattern = /^\s+\S/
 const commentPattern = /^\s*\/\/(.*)/
 const testPattern = /^export function (?:only_|skip_)?test_([\w_]+)/
 const exportPattern = /^export (?:default )?(?:function|const|let) (\S)+/
+const expectHeadPattern = /^\s*expect\(/
+const expectFullPattern = /^\s*expect\(([^]+)\)\s*.toEqual\(([^]+)\)\s*/
+const asiLinePattern = /^\s*(?:let|const|var|function|class|if|while|;)/
 
-function * test (lines) {
+function * expect (lines) {
+  const buffer = [lines.shift()]
   while (lines.length) {
-    const line = lines[0]
-    if (line.match(/^}/)) {
-      yield * h('code', '', line)
-      lines.shift()
-      return
-    }
-    if (line.match(commentPattern)) {
-      yield * comment(lines)
+    if (lines[0].match(expectHeadPattern) || lines[0].match(commentPattern) ||
+      lines[0].match(asiLinePattern) || lines[0].match(/^}/)) {
+      break
     } else {
-      yield * h('code', '', line)
-      lines.shift()
+      buffer.push(lines.shift())
     }
+  }
+  const text = buffer.join('\n')
+  const match = text.match(expectFullPattern)
+  if (match) {
+    yield * h('code', 'class="expect"', match[1].trim())
+    yield * h('code', 'class="to-equal"', match[2].trim())
+  } else {
+    yield * h('code', 'class="to-equal"', text.trim())
   }
 }
 
-function * exportBody (lines) {
+function * test (lines) {
+  const buffer = []
+  const title = lines.shift().match(testPattern)
+
+  while (lines.length) {
+    const line = lines[0]
+    if (line.match(/^}/)) {
+      lines.shift()
+      break
+    }
+    if (line.match(commentPattern)) {
+      buffer.push(join(comment(lines)))
+    } else if (line.match(expectHeadPattern)) {
+      buffer.push(join(expect(lines)))
+    } else {
+      buffer.push(join(h('code', '', line)))
+      lines.shift()
+    }
+  }
+  yield * h('section', `class="test" id="test_${title}"`, [
+    ...h('header', '', title[1].replace(/_/g, ' ')),
+    ...h('div', 'class="container"', join(buffer))
+  ])
+}
+
+function * contentBody (lines) {
+  const buffer = []
+  buffer.push(join(h('code', '', lines.shift())))
   while (lines.length) {
     const line = lines[0]
 
-    if (line.match(commentPattern)) {
-      yield * comment(lines)
-    } else if (line.match(testPattern)) {
-      return
-    } else if (line.match(indentedPattern) || line.match(exportPattern) || line.match(/^}/)) {
-      yield * h('code', '', line)
-      lines.shift()
+    if (line.match(commentPattern) && line.match(indentedPattern)) {
+      buffer.push(join(comment(lines)))
+    } else if (line.match(commentPattern) || line.match(testPattern) || line.match(exportPattern)) {
+      break
     } else {
-      return
+      buffer.push(join(h('code', '', line)))
+      lines.shift()
     }
+  }
+  if (buffer.length === 1) {
+    yield * h('div', 'class="content"', buffer[0])
+  } else {
+    yield * h('details', 'class="content"', [
+      ...h('summary', '', buffer[0]),
+      ...buffer.slice(1)
+    ])
   }
 }
 
@@ -141,26 +219,16 @@ function * body (lines) {
     if (line.match(commentPattern)) {
       yield * comment(lines)
     } else if (match = line.match(testPattern)) {
-      yield * h('section', `class="test" id="test_${match[1]}"`, [
-        ...test(lines)
-      ])
-    } else if (match = line.match(exportPattern)) {
-      yield * h('section', `class="export" id="${match[1]}"`, [
-        ...exportBody(lines)
-      ])
+      yield * test(lines)
     } else {
-      yield * h('details', '', [
-        ...h('section', 'class="details-body"', [
-          ...detailBody(lines)
-        ])
-      ])
+      yield * contentBody(lines)
     }
   }
 }
 
 function join (stringIter) {
   let str = ''
-  for (const line of stringIter) { str += line + '\n' }
+  for (const line of stringIter) { str += line }
   return str
 }
 
